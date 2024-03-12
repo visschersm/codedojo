28e2e2e3f7fa4a112e62ef2ee0c38a1567c58962
Mark Visschers <visschers.m@gmail.com>
2024-03-12 23:31:03 +0100

    first part of the dojo

---
+++ greed/csharp/greed.tests/GreedCalculatorTests.cs
@@ -0,0 +1,26 @@
+﻿namespace greed.tests;
+
+using FluentAssertions;
+
+public class UnitTest1
+{
+    [Theory]
+    [InlineData(100, new int[] { 1 })]
+    [InlineData(200, new int[] { 1, 1 })]
+    [InlineData(1000, new int[] { 1, 1, 1 })]
+    [InlineData(200, new int[] { 2, 2, 2 })]
+    [InlineData(300, new int[] { 3, 3, 3 })]
+    [InlineData(400, new int[] { 4, 4, 4 })]
+    [InlineData(500, new int[] { 5, 5, 5 })]
+    [InlineData(600, new int[] { 6, 6, 6 })]
+    [InlineData(50, new int[] { 5 })]
+    [InlineData(100, new int[] { 5, 5 })]
+    [InlineData(1150, new int[] { 1, 1, 1, 5, 1 })]
+    [InlineData(0, new int[] { 2, 3, 4, 6, 2 })]
+    [InlineData(350, new int[] { 3, 4, 5, 3, 3 })]
+    public void One_ShouldReturn(int expectedResult, int[] dieList)
+    {
+        var result = GreedCalculator.Calculate(dieList.ToList());
+        result.Should().Be(expectedResult);
+    }
+}
---
+++ greed/csharp/greed/GreedCalculator.cs
@@ -0,0 +1,22 @@
+﻿namespace greed;
+
+public static class GreedCalculator
+{
+    public static int Calculate(List<int> dieList)
+    {
+        var result = dieList.CountBy(x => x).Sum(x =>
+        {
+            return x switch
+            {
+                { Key: 1 } => x.Value / 3 * 1000 + x.Value % 3 * 100,
+                { Key: 5, Value: 3 } => 500,
+                { Key: 5 } => x.Value * 50,
+                { Value: 3 } => x.Key * 100,
+                _ => 0,
+            };
+        });
+
+
+        return result;
+    }
+}
